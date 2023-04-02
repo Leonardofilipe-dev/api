113d283e4e8e3e917c0b46b5627d0d54a77d6aab
Leonardo-dev <leonardo.filipe@yahoo.com>
2023-04-02 20:31:32 -0300

    Validando o cep

--- 01-viacep/js/main.js
+++ 01-viacep/js/main.js
@@ -1,29 +1,55 @@
 
+
+const limparFormulario = (endereco) => {
+
+  
+  document.querySelector("#logradouro").value = ''
+  
+  document.querySelector("#bairro").value = ''
+  
+  document.querySelector("#cidade").value = ''
+  
+  document.querySelector("#estado").value = ''
+
+}
+
 const preencherFormulario = (endereco) => {
+
   // Preencher Rua
   document.querySelector("#logradouro").value = endereco.logradouro
   //Preencher Bairro
   document.querySelector("#bairro").value = endereco.bairro
   //Preencher Cidade
   document.querySelector("#cidade").value = endereco.localidade
-//Preencher estado
+  //Preencher estado
   document.querySelector("#estado").value = endereco.uf
 
 }
 
+const cepValido = (cep) => cep.length == 8
+
 const buscarCep = async () => {
-    const cep = document.querySelector("#cep").value;
-    const url = `http://viacep.com.br/ws/${cep}/json/`
+  limparFormulario()
+  const cep = document.querySelector("#cep").value;
+  const url = `http://viacep.com.br/ws/${cep}/json/`
+  if (cepValido(cep)) {
     const prom = await fetch(url)
     const endereco = await prom.json()
-    if (endereco.hasOwnProperty('erro')){
+    if (endereco.hasOwnProperty('erro')) {
       alert("Inclua um cep valido!")
-    }else
-    preencherFormulario(endereco)
-
+    } else
+      preencherFormulario(endereco)
+  } else {
+    document.querySelector("#logradouro").value = "Cep não encontrado"
   }
 
+  const prom = await fetch(url)
+  const endereco = await prom.json()
+  if (endereco.hasOwnProperty('erro')) {
+    alert("Inclua um cep valido!")
+  } else
+    preencherFormulario(endereco)
 
-  
-  document.querySelector("#cep").addEventListener('focusout', buscarCep);
-  
+}
+
+document.querySelector("#cep").addEventListener('focusout', buscarCep);
